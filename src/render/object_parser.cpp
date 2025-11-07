89dcafa2e696b5e632eaa7fc5d675873f0e0b927
Dmitry Kopytkov <d.kopytkov@g.nsu.ru>
2025-11-07 11:49:21 +0700

    Добавлен стол + борты

--- src/render/object_parser.cpp
+++ src/render/object_parser.cpp
@@ -1,6 +1,7 @@
 #include "object_parser.h"
 #include "json_utils.h"
 #include "material.h"
+#include "scene_object_box.h"
 #include "scene_object_sphere.h"
 
 using json = nlohmann::json;
@@ -14,9 +15,9 @@ std::unique_ptr<SceneObject> ObjectParser::Parse(const json& j) {
   }
 
   std::string type = j["type"].get<std::string>();
-  auto pos_arr = parseVec3(j["position"], "object.position");
-  vec3 pos(pos_arr[0], pos_arr[1], pos_arr[2]);
+  vec3 pos = vec3(parseVec3(j["position"], "object.position"));
 
+  // Парсим sphere
   if (type == "sphere") {
     if (!j.contains("radius")) {
       throw std::runtime_error("Missing object.radius");
@@ -26,28 +27,24 @@ std::unique_ptr<SceneObject> ObjectParser::Parse(const json& j) {
       throw std::runtime_error("Invalid radius");
     }
 
-    // Парсим материал
-    Material mat;
-    if (j.contains("material")) {
-      const auto& m = j["material"];
-      if (m.contains("color")) {
-        auto c = m["color"];
-        mat.base_color = RGB{static_cast<uint8_t>(c[0].get<float>() * 255),
-                             static_cast<uint8_t>(c[1].get<float>() * 255),
-                             static_cast<uint8_t>(c[2].get<float>() * 255)};
-      }
-      if (m.contains("reflectivity")) {
-        mat.reflectivity = m["reflectivity"];
-      }
-      if (m.contains("transparency")) {
-        mat.transparency = m["transparency"];
-      }
-      if (m.contains("refraction")) {
-        mat.refraction = m["refraction"];
-      }
-    }
+    Material mat =
+        j.contains("material") ? parseMaterial(j["material"]) : Material{};
 
     return std::make_unique<Sphere>(pos, r, mat);
+  }
+
+  // Парсим box
+  if (type == "box") {
+    if (!j.contains("vertex")) {
+      throw std::runtime_error("Missing object.vertex for box");
+    }
+
+    vec3 vertex = vec3(parseVec3(j["vertex"], "object.vertex"));
+
+    Material mat =
+        j.contains("material") ? parseMaterial(j["material"]) : Material{};
+
+    return std::make_unique<Box>(pos, vertex, mat);
   }
 
   throw std::runtime_error("Unknown object type: " + type);
